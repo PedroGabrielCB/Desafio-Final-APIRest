48839608061616ccbfd72168ebc78e66db3a1473
PedroGabrielCB <contato.pedrogabrielcb@gmail.com>
2023-06-25 17:54:30 -0300

    correção route index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Desafio Final API Rest' });
 });
 
 module.exports = router;
